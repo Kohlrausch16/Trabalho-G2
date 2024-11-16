ca09fbe995892bcc2df21b5bb47a2f3f0a62bd06
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2024-11-16 19:08:02 -0300

    Add nav-bar component
    Adicionado componente nav-bar

--- store/src/App.js
+++ store/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-        <p>
-          Hello
-        </p>
+      <Navbar />
     </div>
   );
 }
---
+++ store/src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <div className="navbar-logo">
+            <a href="/"><span>My</span>Store</a>
+          </div>
+          <ul className="navbar-links-left">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Produtos</a></li>
+          </ul>
+        </div>
+        <ul className="navbar-links-right">
+            <input class="input-search" ></input>
+            <a href="/"><i class="fa-solid fa-magnifying-glass fa-lg"></i></a>
+            <li><a href="/">Login</a></li>
+            <a href="/"><i class="fa-solid fa-cart-shopping fa-lg"></i></a>
+
+        </ul>
+      </nav>
+      <hr className="navbar-divider" />
+    </>
+    );
+  };
+  
+export default Navbar;
+  
