8141783021524fd849aeac09b0ad38fd214ba00a
João Henrique Kohlrausch <145872345+Kohlrausch16@users.noreply.github.com>
2024-11-17 21:58:28 -0300

    Add files via upload

---
+++ store/src/components/SeletorQuantidade.jsx
@@ -0,0 +1,10 @@
+import "./SeletorQuantidade.css";
+
+
+function SeletorQuantidade(){
+    return (
+        <input type="number" className="SeletorQuantidade" />
+    );
+}
+
+export default SeletorQuantidade;
