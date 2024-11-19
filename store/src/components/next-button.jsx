33d538f9f1aa375606496c2cc699021226a3c65e
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2024-11-19 20:10:18 -0300

    Update next-button.jsx

--- store/src/components/next-button.jsx
+++ store/src/components/next-button.jsx
@@ -4,7 +4,7 @@ import './next-button.css';
 const NextButton = () => {
     return (
         <>
-       <a href="/login/PersonalAnddress"> <button id='NextButton' type='submit' >Avançar  <i id='arrow' className="fa-solid fa-chevron-right fa-sm"></i></button></a>
+       <a href="/"> <button id='NextButton' type='submit' >Avançar  <i id='arrow' className="fa-solid fa-chevron-right fa-sm"></i></button></a>
     </>
     );
   };
